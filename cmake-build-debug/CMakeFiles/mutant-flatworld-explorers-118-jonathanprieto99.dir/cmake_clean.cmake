e358a88c10186fc5b99ed82f97e9b3ec80c77108
Jonathan Prieto <jonathan.prieto@me.com>
2019-09-18 00:07:52 -0500

    First Upload

---
+++ cmake-build-debug/CMakeFiles/mutant-flatworld-explorers-118-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mutant-flatworld-explorers-118-jonathanprieto99.dir/main.cpp.o"
+  "mutant-flatworld-explorers-118-jonathanprieto99.pdb"
+  "mutant-flatworld-explorers-118-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mutant-flatworld-explorers-118-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/mutant-flatworld-explorers-118-jonathanprieto99.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mutant-flatworld-explorers-118-jonathanprieto99.
+# This may be replaced when dependencies are built.
