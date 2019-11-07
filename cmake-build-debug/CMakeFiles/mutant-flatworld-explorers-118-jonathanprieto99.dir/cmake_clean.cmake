965480340461f1b14710f03711ba63d48cc4d486
Jonathan Prieto <jonathan.prieto@me.com>
2019-11-07 00:16:34 -0500

    Se agrego string.h

--- cmake-build-debug/CMakeFiles/mutant-flatworld-explorers-118-jonathanprieto99.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/mutant-flatworld-explorers-118-jonathanprieto99.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/mutant-flatworld-explorers-118-jonathanprieto99.dir/main.cpp.o"
-  "mutant-flatworld-explorers-118-jonathanprieto99.pdb"
   "mutant-flatworld-explorers-118-jonathanprieto99"
+  "mutant-flatworld-explorers-118-jonathanprieto99.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
